c2567ffe95aa37b7ec7f6cacd8873478cd80c70c
Hunter Lacefield <lace1010@gmail.com>
2021-02-17 14:46:17 +0800

    Added comments to keep code from previous challenges without messing with new challenge (This way I have notes)

--- myApp.js
+++ myApp.js
@@ -1,10 +1,16 @@
 var express = require("express");
 var app = express();
 
+// First challenge in basic node and express
 console.log("Hello World");
 
+/*  Seond challenge in basic node and express
+app.get("/", function (req, res) {
+  res.sendFile("Hello Express");
+}); */
+
+// Third Challenge in basic node and express
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
-
 module.exports = app;
