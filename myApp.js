56a3810d63c06ebee160506accb608407b1b149e
Hunter Lacefield <lace1010@gmail.com>
2021-02-17 17:14:16 +0800

    Serve JSON object with node in myApp.js

--- myApp.js
+++ myApp.js
@@ -14,6 +14,13 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// Fourth challenge in basic node and express -Serve Static Assests
+// Fourth challenge in basic node and express -Serve Static Assests (middleware functions)
+// Middleware syntax - app.use(path, middlewareFunction).
+// We must have the "/public" in path before calling express.static for this to work
 app.use("/public", express.static(__dirname + "/public"));
 module.exports = app;
+
+// Fifth challenge in basic node and express -Serve JSON on a specific route
+app.get("/json", (req, res) => {
+  res.json({ message: "Hello json" });
+});
