c32344bad181b8fd8cb1129ef2d2f828c787b9f1
Hunter Lacefield <lace1010@gmail.com>
2021-02-17 15:10:54 +0800

    Add static assest (css) with app.use(express.static())

--- myApp.js
+++ myApp.js
@@ -1,16 +1,19 @@
 var express = require("express");
 var app = express();
 
-// First challenge in basic node and express
+// First challenge in basic node and express -log Hello World
 console.log("Hello World");
 
-/*  Seond challenge in basic node and express
+/*  Seond challenge in basic node and express - Send a file (string)
 app.get("/", function (req, res) {
   res.sendFile("Hello Express");
 }); */
 
-// Third Challenge in basic node and express
+// Third challenge in basic node and express -send HTML file
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+// Fourth challenge in basic node and express -Serve Static Assests
+app.use(express.static(__dirname + "/public"));
 module.exports = app;
