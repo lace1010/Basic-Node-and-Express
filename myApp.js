f4310e25707c5d69d60f6b9c175417f3b95cb1cf
Hunter Lacefield <lace1010@gmail.com>
2021-02-18 14:33:39 +0800

    Add dot env package and .env file and connect it to myAPP.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+require("dotenv").config();
 
 // First challenge in basic node and express -log Hello World
 console.log("Hello World");
@@ -10,7 +11,7 @@ app.get("/", function (req, res) {
 }); */
 
 // Third challenge in basic node and express -send HTML file
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
@@ -20,7 +21,19 @@ app.get("/", function (req, res) {
 app.use("/public", express.static(__dirname + "/public"));
 module.exports = app;
 
-// Fifth challenge in basic node and express -Serve JSON on a specific route
+/* Fifth challenge in basic node and express -Serve JSON on a specific route
 app.get("/json", (req, res) => {
+  // This means if we add /json to the end of the url of our website the json object will be shown on the screen
   res.json({ message: "Hello json" });
+}); */
+
+// Sixth challenge in basic node and express -use the .env File
+// need to create a .env file for this to work.
+app.get("/json", (req, res) => {
+  // Make sure uppercase is a string inside quotes...
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "hello json" });
+  }
 });
