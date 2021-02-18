ac88691e5db05fd0ad3a8e2a58ad816d95df916a
Hunter Lacefield <lace1010@gmail.com>
2021-02-18 23:21:04 +0800

    Chain middleware functions to create a time server

--- myApp.js
+++ myApp.js
@@ -4,38 +4,38 @@ var app = express();
 require("dotenv").config(); // Be sure to put this at top of page to load first
 // Make sure to add config variables to heroku in the settings config vars section
 
-// First challenge in basic node and express -log Hello World
-console.log("Hello World");
-
-// Seventh challenge in basic node and express -Implement a Root-Level Request Logger Middleware
-// This challenge has to be placed before routes in order to work
+// --> 7) Mount the logger middleware here
+// We put this logger middlware first because we want it to log for all routes.
 app.use((req, res, next) => {
   console.log(req.method + " " + req.path + " - " + req.ip);
   next(); // If next() is not called then your servel will be stuck forever. (Essentially ends the loop)
 });
 
-/*  Seond challenge in basic node and express - Send a file (string)
+//** 1) -log Hello World
+console.log("Hello World");
+
+/** 2) First Working Express Server (Send a file (string))
 app.get("/", function (req, res) {
   res.sendFile("Hello Express");
 }); */
 
-// Third challenge in basic node and express -send HTML file
+//** 3) Serve an HTML file
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// Fourth challenge in basic node and express -Serve Static Assests (middleware functions)
+//** 4) Serve Static Assests (middleware functions)
 // Middleware syntax - app.use(path, middlewareFunction).
 // We must have the "/public" in path before calling express.static for this to work
 app.use("/public", express.static(__dirname + "/public"));
 
-/* Fifth challenge in basic node and express -Serve JSON on a specific route
+/** 5) Serve JSON on a specific route
 app.get("/json", (req, res) => {
   // This means if we add /json to the end of the url of our website the json object will be shown on the screen
   res.json({ message: "Hello json" });
 }); */
 
-// Sixth challenge in basic node and express - use the .env File
+//** 6) use the .env File
 // need to create a .env file for this to work.
 console.log(process.env.MESSAGE_STYLE, " <= message style");
 app.get("/json", (req, res) => {
@@ -47,7 +47,19 @@ app.get("/json", (req, res) => {
   res.json(jsonResponse);
 });
 
-// Eigth challenge in basic node and express -Chain Middleware to Create a Time Server
+//** 7) Implement a Root-Level Request Logger Middleware
+// This challenge has to be placed before routes in order to work
 
+//** 8) Chain Middleware to Create a Time Server
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
 // This last line of code was here when I cloned it over
 module.exports = app;
